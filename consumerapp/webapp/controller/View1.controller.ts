9433ffa493184f41311c64dd6b853dd3998c8d11
Mio <mio.fujita.01@gmail.com>
2026-05-23 06:13:47 +0900

    use library in consumerapp

--- consumerapp/webapp/controller/View1.controller.ts
+++ consumerapp/webapp/controller/View1.controller.ts
@@ -1,4 +1,7 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import MyDialogHanlder, { MyDialogHandler$SubmitEvent } from "com/myorg/reuselib/controller/MyDialogHandler";
+import MessageToast from "sap/m/MessageToast";
+import Log from "sap/base/Log";
 
 /**
  * @namespace miyasuta.consumerapp.controller
@@ -6,6 +9,15 @@ import Controller from "sap/ui/core/mvc/Controller";
 export default class View1 extends Controller {
 
     public onInit(): void {
+        Log.info("View1 controller initialized");
+    }
 
+    public onOpenDialog(): void {
+        const dialogHandler = new MyDialogHanlder();
+        dialogHandler.attachSubmit((event: MyDialogHandler$SubmitEvent) => {
+            const message = event.getParameter("message");
+            MessageToast.show(`Dialog submitted with message: ${message}`);
+        });
+        dialogHandler.open();
     }
 }
